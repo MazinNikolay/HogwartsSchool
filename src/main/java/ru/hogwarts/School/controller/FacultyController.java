eea6c6a0a8aecf16e245cedc4b5919cdde33ae78
kolia <koliamba_85@mail.ru>
2024-04-14 17:37:06 +0800

    Добавил аннотации Tag и Operation в контроллеры

--- src/main/java/ru/hogwarts/School/controller/FacultyController.java
+++ src/main/java/ru/hogwarts/School/controller/FacultyController.java
@@ -1,6 +1,7 @@
 package ru.hogwarts.School.controller;
 
-import org.springframework.http.ResponseEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 import ru.hogwarts.School.model.Faculty;
 import ru.hogwarts.School.service.interfaces.FacultyService;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("faculty")
+@Tag(name = "API для работы с факультетами")
 public class FacultyController {
     private final FacultyService service;
 
@@ -17,26 +19,31 @@ public class FacultyController {
     }
 
     @PostMapping
+    @Operation(summary = "Создание факультета")
     public Faculty createFaculty(@RequestBody Faculty faculty) {
         return service.createFaculty(faculty);
     }
 
     @GetMapping("{id}")
+    @Operation(summary = "Получение факультета")
     public Faculty getFaculty(@PathVariable Long id) {
         return service.getFaculty(id);
     }
 
     @PutMapping
+    @Operation(summary = "Обновление факультета")
     public Faculty updatefaculty(@RequestBody Faculty faculty) {
         return service.updateFaculty(faculty);
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "Удаление факультета")
     public Faculty deletefaculty(@PathVariable Long id) {
         return service.deleteFaculty(id);
     }
 
     @GetMapping("/sortedByColor/{color}")
+    @Operation(summary = "Получение факультетов по цвету")
     public List<Faculty> sortedByColor(@PathVariable String color) {
         return service.sortByColor(color);
     }
--- src/main/java/ru/hogwarts/School/controller/StudentController.java
+++ src/main/java/ru/hogwarts/School/controller/StudentController.java
@@ -1,6 +1,7 @@
 package ru.hogwarts.School.controller;
 
-import org.springframework.http.ResponseEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 import ru.hogwarts.School.model.Student;
 import ru.hogwarts.School.service.interfaces.StudentService;
@@ -9,6 +10,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("student")
+@Tag(name = "API для работы со студентами")
 public class StudentController {
     private final StudentService service;
 
@@ -17,21 +19,25 @@ public class StudentController {
     }
 
     @PostMapping
+    @Operation(summary = "Создание студента")
     public Student createStudent(@RequestBody Student student) {
         return service.createStudent(student);
     }
 
     @GetMapping("{id}")
+    @Operation(summary = "Получение студента")
     public Student getStudent(@PathVariable Long id) {
         return service.getStudent(id);
     }
 
     @PutMapping
+    @Operation(summary = "Обновление студента")
     public Student updateStudent(@RequestBody Student student) {
         return service.updateStudent(student);
     }
 
     @DeleteMapping("{id}")
+    @Operation(summary = "Удаление студента")
     public Student deleteStudent(@PathVariable Long id) {
         return service.deleteStudent(id);
     }
@@ -42,6 +48,7 @@ public class StudentController {
     }*/
 
     @GetMapping("/sortedByAge/{age}")
+    @Operation(summary = "Получение студентов по возрасту")
     public List<Student> sortedByAge(@PathVariable int age) {
         return service.sortByAge(age);
     }
